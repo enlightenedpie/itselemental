e550a6dbf0c65727a9ea827b8cfb3a6a954afdb7
David Paul <enlightenedpie@gmail.com>
2020-08-22 18:39:24 -0500

    commiting basic layout to branch

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,31 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { v4 as uuidv4 } from 'uuid'
+import Element from '../components/Element'
+
+import data from '../data/pte'
+
+const getKey = () => {
+  return uuidv4()
+}
 
 export default function Home() {
+  let { elements: els } = data
+  console.log(els)
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <main>
+      {els.map((el) => {
+        return (
+          <div
+            key={getKey()}
+            className="el"
+            id={'el' + el.number}
+            style={{
+              gridColumn: `${el.xpos} / ${el.xpos + 1}`,
+              gridRow: `${el.ypos} / ${el.ypos + 1}`,
+            }}
+          ></div>
+        )
+      })}
+    </main>
   )
 }
